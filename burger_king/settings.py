56ce0e627da8cef135bc627344a50e9a219e51e4
gulmira <g.djumadylova@gmail.com>
2022-02-21 15:51:19 +0600

    *

--- burger_king/settings.py
+++ burger_king/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')   # config ищет секретный клю�
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
